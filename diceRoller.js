04c5e9602a9c4d9a3177cad7ee040ed2a6d821cc
ClintasaurusRex <clinthiles1992@gmail.com>
2026-08-23 13:26:08 -0700

    updated diceRoller

---
+++ diceRoller.js
@@ -0,0 +1,14 @@
+const args = process.argv.slice(2);
+const count = Number(args[1] ?? 1); // This says if this does not exist make it 1
+const sides = Number(args[0]);
+
+if (Number.isNaN(sides) || !Number.isInteger(sides) || sides < 2) {
+  console.log("Please provide a valid number of sides.");
+} else {
+  for (let i = 0; i < count; i++) {
+    const roll = Math.floor(Math.random() * sides) + 1;
+    console.log(`🎲 You rolled ${roll}/${sides}`);
+  }
+}
+
+// console.log(typeof sides);
