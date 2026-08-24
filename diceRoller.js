cbdd951d6b1adb423675ac7e4a366be35569113e
ClintasaurusRex <clinthiles1992@gmail.com>
2026-08-23 19:49:41 -0700

    Added in validation for the roll counter

--- diceRoller.js
+++ diceRoller.js
@@ -4,6 +4,8 @@ const sides = Number(args[0]);
 
 if (Number.isNaN(sides) || !Number.isInteger(sides) || sides < 2) {
   console.log("Please provide a valid number of sides.");
+} else if (Number.isNaN(count) || !Number.isInteger(count) || count < 1) {
+  console.log("Please provide a valid roll count.");
 } else {
   for (let i = 0; i < count; i++) {
     const roll = Math.floor(Math.random() * sides) + 1;
